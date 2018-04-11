dbbabddaf5498ba9d28380cfd8d6d9d61b0c130a
Ryan Cloutier <cloutier@astro.utoronto.ca>
2018-04-11 10:23:52 -0400

    keep saved version of runrvfc_FULL as i test why runrvfc is failing

--- php2python.py
+++ php2python.py
@@ -2,10 +2,10 @@ import os, sys
 from RVFollowupCalculator import *
 
 
-def update_input_files(wlmin, wlmax, R, aperture, throughput, floor, wlcen, targetsnr,
-                       maxtelluric, mintexp, maxtexp, overhead, sigRVphot,
-                       sigRVact, sigRVplanets, sigRVeff, P, rp, mp, mags, Ms,
-                       Rs, Teff, Z, vsini, Prot):
+def update_input_files(wlmin, wlmax, R, aperture, throughput, floor, maxtelluric, overhead, 
+		       texp, sigRVphot, sigRVact, sigRVplanets, sigRVeff, 
+		       P, rp, mp, 
+		       mags, Ms, Rs, Teff, Z, vsini, Prot):
     # update spectrograph
     f = open('InputFiles/user_spectrograph_template.in', 'r')
     g = f.read()
@@ -16,11 +16,7 @@ def update_input_files(wlmin, wlmax, R, aperture, throughput, floor, wlcen, targ
     g = g.replace('<<aperture>>', '%.2f'%aperture)
     g = g.replace('<<throughput>>', '%.2f'%throughput)
     g = g.replace('<<floor>>', '%.2f'%floor)
-    g = g.replace('<<wlcen>>', '%.2f'%wlcen)
-    g = g.replace('<<targetsnr>>', '%.2f'%targetsnr)
     g = g.replace('<<maxtelluric>>', '%.2f'%maxtelluric)
-    g = g.replace('<<mintexp>>', '%.2f'%mintexp)
-    g = g.replace('<<maxtexp>>', '%.2f'%maxtexp)
     g = g.replace('<<overhead>>', '%.2f'%overhead)
     h = open('InputFiles/user_spectrograph.in', 'w')
     h.write(g)
@@ -30,6 +26,7 @@ def update_input_files(wlmin, wlmax, R, aperture, throughput, floor, wlcen, targ
     f = open('user_sigRV_template.in', 'r')
     g = f.read()
     f.close()
+    g = g.replace('<<texp>>', '%.3f'%texp)
     g = g.replace('<<sigRVphot>>', '%.4f'%sigRVphot)
     g = g.replace('<<sigRVact>>', '%.4f'%sigRVact)
     g = g.replace('<<sigRVplanets>>', '%.4f'%sigRVplanets)
@@ -78,38 +75,41 @@ def run_calculator(Kdetsig, duration=100, NGPtrials=1):
 
 
 if __name__ == '__main__':
-    bands = list(sys.argv[1])
-    R = float(sys.argv[2])
-    aperture = float(sys.argv[3])
-    throughput = float(sys.argv[4])
-    floor = float(sys.argv[5])
-    wlcen = float(sys.argv[6])
-    targetsnr = float(sys.argv[7])
-    maxtelluric = float(sys.argv[8])
-    mintexp = float(sys.argv[9])
-    maxtexp = float(sys.argv[10])
-    overhead = float(sys.argv[11])
-    sigRVphot = float(sys.argv[12])
-    sigRVact = float(sys.argv[13])
-    sigRVplanets = float(sys.argv[14])
-    sigRVeff = float(sys.argv[15])
-    P = float(sys.argv[16])
-    rp = float(sys.argv[17])
-    mp = float(sys.argv[18])
-    mags = list(sys.argv[19])
-    Ms = float(sys.argv[20])
-    Rs = float(sys.argv[21])
-    Teff = float(sys.argv[22])
-    Z = float(sys.argv[23])
-    vsini = float(sys.argv[24])
-    Prot = float(sys.argv[25])
-    Kdetsig = float(sys.argv[26])
-    NGPtrials = float(sys.argv[27])
-    runGP = int(sys.argv[28])
-    
-    update_input_files(bands, R, aperture, throughput, floor, wlcen, targetsnr,
-                       maxtelluric, mintexp, maxtexp, overhead, sigRVphot,
-                       sigRVact, sigRVplanets, sigRVeff, P, rp, mp, mags, Ms,
-                       Rs, Teff, Z, vsini, Prot):
+    assert len(sys.argv) == 26
+
+    wlmin = float(sys.argv[1])
+    wlmax = float(sys.argv[2])
+    R = float(sys.argv[3])
+    aperture = float(sys.argv[4])
+    throughput = float(sys.argv[5])
+    floor = float(sys.argv[6])
+    maxtelluric = float(sys.argv[7])
+    overhead = float(sys.argv[8])
+
+    texp = float(sys.argv[9])
+    sigRVphot = float(sys.argv[10])
+    sigRVact = float(sys.argv[11])
+    sigRVplanets = float(sys.argv[12])
+    sigRVeff = float(sys.argv[13])
+
+    P = float(sys.argv[14])
+    rp = float(sys.argv[15])
+    mp = float(sys.argv[16])
+
+    mags = list(sys.argv[17])
+    Ms = float(sys.argv[18])
+    Rs = float(sys.argv[19])
+    Teff = float(sys.argv[20])
+    Z = float(sys.argv[21])
+    vsini = float(sys.argv[22])
+    Prot = float(sys.argv[23])
+
+    Kdetsig = float(sys.argv[24])
+    NGPtrials = float(sys.argv[25])
+   
+    update_input_files(wlmin, wlmax, R, aperture, throughput, floor, maxtelluric, overhead, 
+		       texp, sigRVphot, sigRVact, sigRVplanets, sigRVeff,
+		       P, rp, mp, 
+		       mags, Ms, Rs, Teff, Z, vsini, Prot)
 
     #run_calulator(Kdetsig, NGPtrials=NGPtrials)
