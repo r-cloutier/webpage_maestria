e0a4b618ca20f17c40269efebf55da5d798ab73a
Ryan Cloutier <cloutier@astro.utoronto.ca>
2018-05-08 17:28:44 -0400

    cleaned the text up a bit

--- option1.php
+++ option1.php
@@ -2,15 +2,15 @@
 <p style="font-size:24px" align="center">&nbsp;&nbsp;&nbsp;<b>Option 1: RVFC-derived RV precision</b></p>&nbsp;&nbsp;&nbsp;
 
             <div style="padding-left:160px;padding-right:160px">
-		Option 1 should be used when the photon-noise limited RV precision and the effective RV rms are unknown for the target of
-		interest.
+		To-be used when the photon-noise limited RV precision and the effective RV rms are unknown for the target of interest.
 	    </div>
 	    <div style="padding-left:180px;padding-right:160px">
+	    	<br><h5>Select a suboption:</h5>
 	        <br>
-			<input type="submit" name="submit_1d1" style="font-weight:normal;" value="Option 1.1"/> <br>
-			Select <b>option 1.1</b> if no astrophysical RV noise sources are known. In this case the RV noise
+			<input type="submit" name="submit_1d1" style="font-weight:bold;" value="Option 1.1"/>
+			: select if no astrophysical RV noise sources are known. In this case the RV noise
 			from stellar activity and from additional planets will be sampled from appropriate empirical distributions.<br><br><br>
-			<input type="submit" name="submit_1d2" style="font-weight:normal;" value="Option 1.2"/><br>
-			Select <b>option 1.2</b> to set the individual astrophysical RV noise sources explicitly.
+			<input type="submit" name="submit_1d2" style="font-weight:bold;" value="Option 1.2"/>
+			: select to set the individual astrophysical RV noise sources explicitly.
 	    </div>
 
--- option2.php
+++ option2.php
@@ -2,18 +2,18 @@
 <p style="font-size:24px" align="center">&nbsp;&nbsp;&nbsp;<b>Option 2: user-specified RV precision</b></p>&nbsp;&nbsp;&nbsp;
 
             <div style="padding-left:160px;padding-right:160px">
-		Option 2 should be used when either the photon-noise limited RV precision or the effective RV rms are known a-priori for the target 
-		of interest (often from previous runs of the RVFC or from independent calculations). To proceed, select one of the sub-options below 
-		based on the user's prior knowledge of the various RV noise sources.
+		To-be used when the photon-noise limited RV precision or the effective RV rms is known a-priori for the target
+                of interest (often from previous runs of the RVFC or from independent calculations).
 	    </div>
 	    <div style="padding-left:180px;padding-right:160px">
+	        <br><h5>Select a suboption:</h5>	
 	        <br>
-			<input type="submit" name="submit_2d1" style="font-weight:normal;" value="Option 2.1"/> <br>
-			Select <b>option 2.1</b> if no astrophysical RV noise sources are known. In this case the RV noise
+			<input type="submit" name="submit_2d1" style="font-weight:bold;" value="Option 2.1"/> 
+			: select if no astrophysical RV noise sources are known. In this case the RV noise
 			from stellar activity and from additional planets will be sampled from appropriate empirical distributions.<br><br><br>
-			<input type="submit" name="submit_2d2" style="font-weight:normal;" value="Option 2.2"/><br>
-			Select <b>option 2.2</b> to set the individual RV noise sources explicitly.</br><br><br>
-			<input type="submit" name="submit_2d3" style="font-weight:normal;" value="Option 2.3"/><br> 
-			Select <b>option 2.3</b> to set the effective RV rms verbatim thus negating the need to specify any other RV noise source. This
+			<input type="submit" name="submit_2d2" style="font-weight:bold;" value="Option 2.2"/>
+			: select to set the individual RV noise sources explicitly.</br><br><br>
+			<input type="submit" name="submit_2d3" style="font-weight:bold;" value="Option 2.3"/>
+			: select to set the effective RV rms verbatim thus negating the need to specify any other RV noise source. This
 			option is not compatible with GP calculations.
 	    </div>
