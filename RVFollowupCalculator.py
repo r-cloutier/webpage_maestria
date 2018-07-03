6b16026ac6c9b8e829c7788e59340994199ff621
Ryan Cloutier <cloutier@astro.utoronto.ca>
2018-07-03 06:11:55 -0400

    added reporting of the Nrvgp dispersion

--- RVFollowupCalculator.py
+++ RVFollowupCalculator.py
@@ -446,10 +446,8 @@ def _write_results2file(output_fname, magiclistofstuff2write):
 
 
 def _save_RVFC(output, output_fname):
-    P, rp, mp, K, mags, Ms, Rs, Teff, Z, vsini, Prot, band_strs, R, aperture, throughput, RVnoisefloor, centralwl_microns,
-    maxtelluric, toverhead, texp, SNRtarget, sigRV_phot, sigRV_act, sigRV_planet, sigRV_eff, sigK_target, nRV, nRVGP, enRVGP, NGPtrials, tobs, tobsGP, etobsGP = output
-    g =
-    '%.7f,%.4f,%.4f,%s,%s,%.4f,%.4f,%i,%.3f,%.3f,%.3f,%i,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.2f,%.2f,%.2f,%.2f,%.2f,%i,%.1f,%.1f,%.1f,%.3f,%.3f,%.3f'%(P,rp,mp,'-'.join(mags.astype(str)),''.join(band_strs),Ms,Rs,Teff,Z,vsini,Prot,R,aperture,throughput,RVnoisefloor,centralwl_microns,maxtelluric,toverhead,texp,sigRV_phot,sigRV_act,sigRV_planet,sigRV_eff,sigK_target,NGPtrials,nRV,nRVGP,enRVGP,tobs,tobsGP,etobsGP)
+    P, rp, mp, K, mags, Ms, Rs, Teff, Z, vsini, Prot, band_strs, R, aperture, throughput, RVnoisefloor, centralwl_microns, maxtelluric, toverhead, texp, SNRtarget, sigRV_phot, sigRV_act, sigRV_planet, sigRV_eff, sigK_target, nRV, nRVGP, enRVGP, NGPtrials, tobs, tobsGP, etobsGP = output
+    g = '%.7f,%.4f,%.4f,%s,%s,%.4f,%.4f,%i,%.3f,%.3f,%.3f,%i,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.2f,%.2f,%.2f,%.2f,%.2f,%i,%.1f,%.1f,%.1f,%.3f,%.3f,%.3f'%(P,rp,mp,'-'.join(mags.astype(str)),''.join(band_strs),Ms,Rs,Teff,Z,vsini,Prot,R,aperture,throughput,RVnoisefloor,centralwl_microns,maxtelluric,toverhead,texp,sigRV_phot,sigRV_act,sigRV_planet,sigRV_eff,sigK_target,NGPtrials,nRV,nRVGP,enRVGP,tobs,tobsGP,etobsGP)
     f = open(output_fname, 'w')
     f.write(g)
     f.close()
